3e64c76f18b72a5892445bada5452ce8a0c44a6f
Ryan Berckmans <ryanberckmans@gmail.com>
2014-07-06 13:43:43 -0400

    show all todos, show only completed todos, button to clear completed todos

--- app/controllers/todos.js
+++ app/controllers/todos.js
@@ -18,7 +18,13 @@ export default Ember.ArrayController.extend({
 
       // Save the new model
       todo.save();
-    }
+    },
+
+    clearCompleted: function() {
+      var completed = this.filterBy('isCompleted', true);
+      completed.invoke('deleteRecord');
+      completed.invoke('save');
+    },
   },
 
   remaining: function() {
@@ -28,5 +34,13 @@ export default Ember.ArrayController.extend({
   inflection: function() {
     var remaining = this.get('remaining');
     return remaining === 1 ? 'todo' : 'todos';
-  }.property('remaining')
+  }.property('remaining'),
+
+  hasCompleted: function() {
+    return this.get('completed') > 0;
+  }.property('completed'),
+
+  completed: function() {
+    return this.filterBy('isCompleted', true).get('length');
+  }.property('@each.isCompleted')
 });
