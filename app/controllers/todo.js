1cb570584e60d37bfb6f0371a559d2b4793513fb
Ryan Berckmans <ryanberckmans@gmail.com>
2014-07-06 10:43:39 -0400

    allow editing of todo items. Remove an item whose title is edited to the empty string

--- app/controllers/todo.js
+++ app/controllers/todo.js
@@ -5,6 +5,30 @@ export default Ember.ObjectController.extend({
   // Tutorial calls for adding {{#each itemController="todo"}} to tobos.hbs, which should invoke this controller for the nested {{input}} helper.
   // When I set itemController="todo", it breaks.
   // UPDATE: mystery solved. Previously had Ember.Controller.extend, needed Ember.ObjectController.extend.
+
+  actions: {
+    editTodo: function() {
+      this.set('isEditing', true);
+    },
+
+    acceptChanges: function() {
+      this.set('isEditing', false);
+
+      if (Ember.isEmpty(this.get('model.title'))) {
+        this.send('removeTodo');
+      } else {
+        this.get('model').save();
+      }
+    },
+
+    removeTodo: function () {
+      var todo = this.get('model');
+      todo.deleteRecord();
+      todo.save();
+    }
+  },
+
+  isEditing: false,
   
   // The app works correctly when i removed isCompleted computed property. Perhaps because isCompleted is a primitive type, it works out of box.
   isCompleted: function(key, value){
